6f19d49cffde47056917ec12bbbc4a10cea62da4
Abhiraj Patel <patelabhiraj222@gmail.com>
2023-10-22 19:56:23 -0500

    intro to js code is added

---
+++ js.1.js
@@ -0,0 +1,26 @@
+// difference 
+
+// const
+ // const a = 5; // declaring and const is same otherwise you will get error but you do it with let and var
+
+let name  = "Masai";
+  name = "school" ;// reassigning the variable name
+console.log(name);
+
+
+// const place = "Delhi";
+// place = "Goa" // you cannot reassign the variable declare with const 
+// but we can do with let and var
+// console.log(place)
+
+// diff between let and var
+// try to use always let
+// scope global, local and block
+
+// let vs var
+
+// console.log(a)
+// var a = 5; // intialize and assign value
+
+// console.log(a)
+// let a = 5;
---
+++ js.3.js
@@ -0,0 +1,3 @@
+let num = 15;
+num = "20"
+let ans = +(20)
